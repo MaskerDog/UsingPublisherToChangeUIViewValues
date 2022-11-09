6e34adf303f3c559ed2aa0c9a4adb3004329e173
MaskerDog <37284851+MaskerDog@users.noreply.github>
2022-11-09 10:28:23 +0900

    Initial Commit

---
+++ Publisher/PublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublisherApp.swift
+//  Publisher
+//
+//  Created by npc on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct PublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
